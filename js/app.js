7dcb35e13dcc7248921c8661971d8ed62869e5fa
Ernest Alilovic <ernest.alilovic@gmail.com>
2018-04-13 16:03:10 +0200

    Added timer and move counter functionality

--- js/app.js
+++ js/app.js
@@ -49,7 +49,24 @@ function init() {
         [].forEach.call(cards, function(item) {
             deck.appendChild(item);
         });
+        cards[i].classList.remove("show", "open", "match", "disabled");
     }
+    // resets moves
+    moves = 0;
+    counter.innerHTML = moves;
+    // resets rating
+    for (var i= 0; i < stars.length; i++) {
+        stars[i].style.color = "#FFD700";
+        stars[i].style.visibility = "visible";
+    }
+    // resets timer
+    second = 0;
+    minute = 0;
+    hour = 0;
+    var timer = document.querySelector(".timer");
+    timer.innerHTML = "0 mins 0 secs";
+    clearInterval(interval);
+}
 
 // adds opened cards to OpenedCards list and check if cards are match or not
 function cardOpen() {
@@ -104,12 +121,49 @@ function enable() {
     });
 }
 
-/*
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// counts users's moves
+function moveCounter() {
+    moves++;
+    counter.innerHTML = moves;
+    //start timer on first click
+    if(moves == 1) {
+        second = 0;
+        minute = 0;
+        hour = 0;
+        startTimer();
+    }
+    // rates user based on moves
+    if (moves > 8 && moves < 12) {
+        for(i= 0; i < 3; i++) {
+            if(i > 1) {
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+    else if (moves > 13) {
+        for(i= 0; i < 3; i++) {
+            if(i > 0) {
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+}
+
+// times user's gameplay
+var second = 0, minute = 0; hour = 0;
+var timer = document.querySelector(".timer");
+var interval;
+function startTimer() {
+    interval = setInterval(function() {
+        timer.innerHTML = minute+"mins "+second+"secs";
+        second++;
+        if(second == 60) {
+            minute++;
+            second = 0;
+        }
+        if(minute == 60) {
+            hour++;
+            minute = 0;
+        }
+    },1000);
+}
